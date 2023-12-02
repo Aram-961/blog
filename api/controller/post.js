148c5592dc57fa4163ff48c0ef27b50ae2259b61
Aram-961 <aramistamboulian961@gmail.com>
2023-12-02 21:25:16 +0200

    working on users posts functionallity

--- api/controller/post.js
+++ api/controller/post.js
@@ -1,3 +1,35 @@
 import express from "express";
+import { db } from "../database.js";
 
 const app = express.Router();
+
+export const getPosts = (req, res) => {
+  // condition checking if there's category in db
+  const SelectFromAllPosts = req.query.category
+    ? "SELECT * FROM posts WHERE category = ?"
+    : // if there's no category show all posts
+      "SELECT * FROM posts";
+  // DATABASE
+  db.query(SelectFromAllPosts, [req.query.category], (err, data) => {
+    if (err) return res.json(err);
+
+    // else return the data
+    return res.status(200).json(data);
+  });
+};
+
+export const getPost = (req, res) => {
+  res.json("getting post from controller");
+};
+
+export const addPost = (req, res) => {
+  res.json("getting post from controller");
+};
+
+export const deletePost = (req, res) => {
+  res.json("getting post from controller");
+};
+
+export const updatePost = (req, res) => {
+  res.json("getting post from controller");
+};
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,6 +1,18 @@
 import express from "express";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../controller/post.js";
 
 const router = express.Router();
 
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/", updatePost);
 
 export default router;
